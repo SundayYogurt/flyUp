bae74f6ee776b9883b87175c8fcf67b220909df1
SundayYogurt <kritsanalalu5@gmail.com>
2026-02-09 14:28:43 +0700

    upperCase

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/flyup/", // เช่น /flyup/
+  base: "/flyUp/", // เช่น /flyup/
 })
